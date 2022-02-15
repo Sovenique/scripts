b99ff9236a3d160543b8ea1a27820cef62b603af
Christos Leventis <83651108+Sovenique@users.noreply.github.com>
2022-02-15 10:40:30 +0200

    Update node_exporter.sh

--- node_exporter.sh
+++ node_exporter.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-mkdir -p /opt/node_exporter && cd /opt/node_exporter && \
+mkdir -p /test/test_exporter/node_exporter && cd /test/test_exporter/node_exporter && \
 wget https://github.com/prometheus/node_exporter/releases/download/v1.0.0-rc.1/node_exporter-1.0.0-rc.1.linux-amd64.tar.gz
 tar -xzf node_exporter-1.0.0-rc.1.linux-amd64.tar.gz && \
 rm -f node_exporter-1.0.0-rc.1.linux-amd64.tar.gz && \
-cat <<EOF> /etc/systemd/system/node_exporter.service
+cat <<EOF> /test/test_exporter/node_exporter/node_exporter.service
 [Unit]
 Description=node_exporter
 Wants=network-online.target
@@ -20,7 +20,4 @@ ExecStart=/opt/node_exporter/node_exporter-1.0.0-rc.1.linux-amd64/node_exporter
 
 WantedBy=multi-user.target
 EOF
-systemctl daemon-reload && \
-systemctl enable node_exporter && \
-systemctl start node_exporter && \
-systemctl status node_exporter
+systemctl daemon-reload 
